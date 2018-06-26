47c6deec0608cfa6d04ced2458ded7bb3a97c65e
Tom <tom.stanley37@gmail.com>
2018-06-26 20:02:42 +0100

    add some echo functionality

--- bot.js
+++ bot.js
@@ -9,10 +9,34 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
-// Create an event listener for messages
+/* Fun and games */
+
+// If someone mentions me, call them out for their good taste in friends
 client.on('message', message => {
   if (message.content.toLowerCase() === 'tom' ) {
-    message.channel.send(`Cool person you're talking about there.`);
+    let author = message.author;
+    message.channel.send(`Cool person you're talking about there, ${author}.`);
+  }
+});
+
+// Looks at the first two letters of the message, sees if they are h and m, if so, adds an m for each letter in the message then adds a fullstop and sends
+client.on('message', message => {
+  const incoming = message.content.toLowerCase();
+  const thinking = incoming.split("");
+  let sarcasticResponse = "H";
+  if (thinking[0] === "h" && thinking[1] === "m" && message.author.username !== "tombot") {
+    for (let i=0; i < thinking.length; i++){
+      sarcasticResponse += "m";
+    }
+  sarcasticResponse += ".";
+  message.channel.send(sarcasticResponse);
+  }
+});
+
+// Memes
+client.on('message', message => {
+  if (message.content.toLowerCase() === 'cod' ) {
+    message.channel.send(`Tom is cod.`);
   }
 });
 
