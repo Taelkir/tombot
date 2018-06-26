1fce1910700c79068618dd14b5d710575187c04a
Tom <tom.stanley37@gmail.com>
2018-06-26 18:09:03 +0100

    add discord credentials to a secret file ignored by git

--- bot.js
+++ bot.js
@@ -1,14 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+// Get the discord API key
+const apiKey = require("./secrets.json");
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
-
-client.login('token');
+console.log(apiKey.client_ID);
+console.log(apiKey.client_secret);
