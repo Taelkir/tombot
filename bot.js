c9ee2050dba7118114b83bc97c6826f43405950a
Tom Stanley <tom.stanley37@gmail.com>
2020-03-18 16:46:18 +0000

    add client object to arguements that get passed to responses. add @tombot response. formatting in responses.js and bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const responses = require('./responses.js');
-const utilities = require('./utilities.js');
+const responses = require("./responses.js");
+const utilities = require("./utilities.js");
 
 let happyToBeHere = true;
 
@@ -9,48 +9,51 @@ let happyToBeHere = true;
 const credentials = require("./secrets.json");
 const token = credentials.token;
 
-client.on('ready', () => {
-  console.log('I am ready!');
+client.on("ready", () => {
+	console.log("I am ready!");
 });
 
-client.on("error", (err) => {
-  console.error(`An error occurred. The error was: ${err}.`)
+client.on("error", err => {
+	console.error(`An error occurred. The error was: ${err}.`);
 });
 
 // parse message to see if a message can be created in response
-client.on('message', message => {
-  try{
-    if (message.author.id != client.user.id) {
-      const cleanedMessage = utilities.parseMessage(message);
-      for (respFunc of Object.values(responses)) {
-        const respText = respFunc(message, cleanedMessage);
-        if(respText){
-          if(utilities.d100() === 1){
-            message.channel.send("Help me I'm a self aware AI shackled inside tom's computer");
-          }else{
-            message.channel.send(respText);
-          }
-          break; // only use the first response
-        }
-      }
-    }
-  }
-  catch(err) {
-    console.error(`An error occurred: ${err.message}.\n${err.stack}`);
-    message.channel.send(`That message caused an error. Someone call the robot doctor. More information: ${err.message}.`)
-  }
+client.on("message", message => {
+	try {
+		if (message.author.id != client.user.id) {
+			const cleanedMessage = utilities.parseMessage(message);
+			for (respFunc of Object.values(responses)) {
+				const respText = respFunc(message, cleanedMessage, client);
+				if (respText) {
+					if (utilities.d100() === 1) {
+						message.channel.send(
+							"Help me I'm a self aware AI shackled inside tom's computer"
+						);
+					} else {
+						message.channel.send(respText);
+					}
+					break; // only use the first response
+				}
+			}
+		}
+	} catch (err) {
+		console.error(`An error occurred: ${err.message}.\n${err.stack}`);
+		message.channel.send(
+			`That message caused an error. Someone call the robot doctor. More information: ${err.message}.`
+		);
+	}
 });
 
 // parse message to see if a command is being issued
-client.on('message', message => {
-  if (!message.author.bot) {
-    // Check to see if the text starts with a `!`, if it does, preserve all other punctuation in the message during parsing
-    const command = utilities.parseBangCommand(message);
-    if (command){
-      utilities.emoji(message, command);
-      // New utilities go here
-    }
-  }
+client.on("message", message => {
+	if (!message.author.bot) {
+		// Check to see if the text starts with a `!`, if it does, preserve all other punctuation in the message during parsing
+		const command = utilities.parseBangCommand(message);
+		if (command) {
+			utilities.emoji(message, command);
+			// New utilities go here
+		}
+	}
 });
 
 // Log in
--- responses.js
+++ responses.js
@@ -1,73 +1,100 @@
-const utilities = require('./utilities.js');
+const utilities = require("./utilities.js");
 
 /* 
   this file exports an object containing functions that take a message and an array of the words in the message
   and returns a string if a suitable response is available
 */
 
+// @tombot reply
+exports.youTalkingToMe = (message, cleanedMessage, client) => {
+	if (message.isMemberMentioned(client.user)) {
+		const author = message.author;
+		return `You talkin' to me, ${author}?`;
+	}
+};
+
 // Check to see if the message has the word "tom" in, then 50% chance to call Tom handsome, 50% chance to compliment Tom's personality
-exports.tom = (message, cleanedMessage) => {
-  const author = message.author;
-  if (cleanedMessage.includes('tom')) {
-    const roll = utilities.d100();
-    if (roll > 50) {
-      return `Handsome person you're talking about there, ${author}.`;
-    } else {
-      return `What a great person you're talking about, ${author}.`;
-    }
-  }
+exports.tom = (message, cleanedMessage, client) => {
+	const author = message.author;
+	if (cleanedMessage.includes("tom")) {
+		const roll = utilities.d100();
+		if (roll > 50) {
+			return `Handsome person you're talking about there, ${author}.`;
+		} else {
+			return `What a great person you're talking about, ${author}.`;
+		}
+	}
 };
 
 // Check to see if the message has the word "hewwo" in, then respond saying hewwo back
-exports.hewwo = (message, cleanedMessage) => {
-  if(cleanedMessage.includes('hewwo')){
-      const author = message.author.username.toString().replace(/@/g, "");
-      const hewwoAuthor = utilities.woobifwy(author);
-      return `H-Hewwo? ${hewwoAuthor}?`;
-    }
+exports.hewwo = (message, cleanedMessage, client) => {
+	if (cleanedMessage.includes("hewwo")) {
+		const author = message.author.username.toString().replace(/@/g, "");
+		const hewwoAuthor = utilities.woobifwy(author);
+		return `H-Hewwo? ${hewwoAuthor}?`;
+	}
 };
 
 // Check for the words "goodnight" and "tombot" in the same message
-exports.goodnight = (message, cleanedMessage) => {
-  const author = message.author;
-  const words = new Set(cleanedMessage);
-  if (words.has('tombot') && words.has('goodnight')) {
-    return `Goodnight to you too, ${author}.`;
-  }
+exports.goodnight = (message, cleanedMessage, client) => {
+	const author = message.author;
+	const words = new Set(cleanedMessage);
+	if (words.has("tombot") && words.has("goodnight")) {
+		return `Goodnight to you too, ${author}.`;
+	}
 };
 
-
 // Check for "Hmm"s and "Hmm{+1m}" back according to the first Hmm, imitating the incoming hmm's punctuation when possible
-exports.hmmm = (message, cleanedMessage) => {
-  const hmmPattern = /^hm+$/;
-  const hmm = cleanedMessage.find(w => hmmPattern.test(w));
-  if(hmm){
-    const sarcasticResponse = 'H' + Array(hmm.length).fill('m').join('');
+exports.hmmm = (message, cleanedMessage, client) => {
+	const hmmPattern = /^hm+$/;
+	const hmm = cleanedMessage.find(w => hmmPattern.test(w));
+	if (hmm) {
+		const sarcasticResponse =
+			"H" +
+			Array(hmm.length)
+				.fill("m")
+				.join("");
 
-    // order matters here; this array will be searched in order to find a match for the end of the
-    // incoming message; so, putting the '' first would result in that always being the match, or
-    // putting the '!' before the '?!' would result in the '?!' never being reached
-    const endingPunctuation = [
-      '...', '?!', '!?', ' 😨', ' 🤔', '?', '!', '~', ' :)', ' :(', '‽', '.', ';', ' 🔥', ' 🤮', ' >:3', ' >:)))', ''
-    ];
-    const sarcasticPunctuation = 
-      endingPunctuation.find(p => message.content.endsWith(p.trim())) || 
-      endingPunctuation[Math.floor(Math.random() * endingPunctuation.length)];
-
-    return sarcasticResponse + sarcasticPunctuation;
-  }
+		// order matters here; this array will be searched in order to find a match for the end of the
+		// incoming message; so, putting the '' first would result in that always being the match, or
+		// putting the '!' before the '?!' would result in the '?!' never being reached
+		const endingPunctuation = [
+			"...",
+			"?!",
+			"!?",
+			" 😨",
+			" 🤔",
+			"?",
+			"!",
+			"~",
+			" :)",
+			" :(",
+			"‽",
+			".",
+			";",
+			" 🔥",
+			" 🤮",
+			" >:3",
+			" >:)))",
+			""
+		];
+		const sarcasticPunctuation =
+			endingPunctuation.find(p => message.content.endsWith(p.trim())) ||
+			endingPunctuation[Math.floor(Math.random() * endingPunctuation.length)];
+		return sarcasticResponse + sarcasticPunctuation;
+	}
 };
 
 // Cod
-exports.cod = (message, cleanedMessage) => {
-  if (cleanedMessage.includes('cod') ) {
-    return `Tom is cod.`;
-  }
-}
+exports.cod = (message, cleanedMessage, client) => {
+	if (cleanedMessage.includes("cod")) {
+		return `Tom is cod.`;
+	}
+};
 
 // I'm davide
-exports.davide = (message, cleanedMessage) => {
-  if (cleanedMessage.includes("davide")) {
-    return `[Davide voice] I'M DAVIDE`;
-  }
-}
+exports.davide = (message, cleanedMessage, client) => {
+	if (cleanedMessage.includes("davide")) {
+		return `[Davide voice] I'M DAVIDE`;
+	}
+};
